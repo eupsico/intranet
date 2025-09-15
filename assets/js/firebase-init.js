8cb86487dbb230820c8d0123f346d7f524d25b0e
marco-egito <eupsico@eupsico.org.br>
2025-09-15 15:59:53 -0300

    Create firebase-init.js

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,26 @@
+// Arquivo: assets/js/firebase-init.js
+// Versão: 1.0
+// Descrição: Arquivo central para configuração e inicialização do Firebase.
+
+// --- CONFIGURAÇÃO ÚNICA DO FIREBASE ---
+const firebaseConfig = {
+    apiKey: "AIzaSyDJqPJjDDIGo7uRewh3pw1SQZOpMgQJs5M",
+    authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+    databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+    projectId: "eupsico-agendamentos-d2048",
+    storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+    messagingSenderId: "1041518416343",
+    appId: "1:1041518416343:web:0a11c03c205b802ed7bb92"
+};
+
+// --- INICIALIZAÇÃO E EXPORTAÇÃO ---
+// Garante que o Firebase seja inicializado apenas uma vez
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+// Exporta as instâncias de autenticação e banco de dados para serem usadas em outros arquivos
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export { auth, db };
