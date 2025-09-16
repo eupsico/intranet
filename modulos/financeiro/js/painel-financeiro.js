d917ed681ec8b3167a7c6795c82ad04799abcc3a
marco-egito <eupsico@eupsico.org.br>
2025-09-16 16:44:57 -0300

    Update painel-financeiro.js

--- modulos/financeiro/js/painel-financeiro.js
+++ modulos/financeiro/js/painel-financeiro.js
@@ -1,8 +1,11 @@
 // Arquivo: assets/js/painel-financeiro.js
-// Versão: 2.7.1
-// Descrição: Corrige o atributo viewBox do ícone de "Voltar".
+// Versão: 2.8
+// Descrição: Remove importação desnecessária de 'firebase-init.js' para corrigir erro 404.
 
 export function initFinancePanel(user, db, userData) {
+    
+    // A variável 'db' já está disponível através dos parâmetros da função.
+    // A linha "import { db } from '...'" foi removida.
     
     window.db = db;
     
@@ -55,7 +58,6 @@ export function initFinancePanel(user, db, userData) {
         sidebarMenu.innerHTML = ''; 
 
         const backLink = document.createElement('li');
-        // CORREÇÃO: viewBox ajustado para "0 0 24 24"
         backLink.innerHTML = `
             <a href="../../../index.html" class="back-link">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
