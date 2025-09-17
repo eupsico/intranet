6a3f555d94181740dca3c747eeeac3e5656f11c5
marco-egito <eupsico@eupsico.org.br>
2025-09-17 14:51:29 -0300

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,6 +1,6 @@
 // Arquivo: assets/js/firebase-init.js
-// Versão: 1.0
-// Descrição: Arquivo central para configuração e inicialização do Firebase.
+// Versão: 1.1
+// Descrição: Adiciona a inicialização e exportação do Firebase Storage.
 
 // --- CONFIGURAÇÃO ÚNICA DO FIREBASE ---
 const firebaseConfig = {
@@ -8,7 +8,7 @@ const firebaseConfig = {
     authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
     databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
     projectId: "eupsico-agendamentos-d2048",
-    storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+    storageBucket: "eupsico-agendamentos-d2048.appspot.com", // Corrigido para o domínio correto do storage
     messagingSenderId: "1041518416343",
     appId: "1:1041518416343:web:0a11c03c205b802ed7bb92"
 };
@@ -19,8 +19,9 @@ if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
-// Exporta as instâncias de autenticação e banco de dados para serem usadas em outros arquivos
+// Exporta as instâncias dos serviços para serem usadas em outros arquivos
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage(); // <-- ALTERAÇÃO 1: Inicializa o Storage
 
-export { auth, db };
+export { auth, db, storage }; // <-- ALTERAÇÃO 2: Exporta o Storage junto com os outros
