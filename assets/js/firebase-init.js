5c3dbafd504879a3db516c969f3ee81c89d1c726
marco-egito <eupsico@eupsico.org.br>
2025-09-26 10:37:23 -0300

    config

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -4,14 +4,15 @@
 
 // --- CONFIGURAÇÃO ÚNICA DO FIREBASE ---
 const firebaseConfig = {
-    apiKey: "AIzaSyDJqPJjDDIGo7uRewh3pw1SQZOpMgQJs5M",
-    authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
-    databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
-    projectId: "eupsico-agendamentos-d2048",
-    storageBucket: "eupsico-agendamentos-d2048.appspot.com",
-    messagingSenderId: "1041518416343",
-    appId: "1:1041518416343:web:0a11c03c205b802ed7bb92"
+  apiKey: "AIzaSyDJqPJjDDIGo7uRewh3pw1SQZOpMgQJs5M",
+  authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+  databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+  projectId: "eupsico-agendamentos-d2048",
+  storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+  messagingSenderId: "1041518416343",
+  appId: "1:1041518416343:web:087006662ffcfa12d7bb92"
 };
+
 
 // --- INICIALIZAÇÃO E EXPORTAÇÃO ---
 // Garante que o Firebase seja inicializado apenas uma vez
